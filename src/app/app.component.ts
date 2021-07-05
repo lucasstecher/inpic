515de21729714334275eb26163c21143c4659e7e
Lucas Stecher <lucas.oliveira@coopersystem.com.br>
2021-07-05 16:33:00 -0300

    testando data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'inpic';
+  dsc = 'Oceano';
+  url = 'https://s2.glbimg.com/sjbIwYbTyJMCcO4JTs_yOSn-SBU=/620x480/e.glbimg.com/og/ed/f/original/2018/11/06/wave-3473335_1920.jpg';
 }
