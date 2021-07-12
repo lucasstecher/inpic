e7db9c775f9b87e121782ad00a1f3964c313fac9
Lucas Stecher <lucasvieira0070@gmail.com>
2021-07-12 09:02:40 -0300

    criação do módulo photo, instalação do bootstrap

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ap-photo',
+    templateUrl: 'photo.component.html'
+})
+
+export class PhotoComponent{
+    dsc = 'Oceano';
+    url = 'https://s2.glbimg.com/sjbIwYbTyJMCcO4JTs_yOSn-SBU=/620x480/e.glbimg.com/og/ed/f/original/2018/11/06/wave-3473335_1920.jpg';
+}
