725271442f603d9bcd30fa6d47a51705c29e8c59
Lucas Stecher <lucas.oliveira@coopersystem.com.br>
2021-07-30 19:12:51 -0300

    data modifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
+  photos = [
+    {
+      url: 'https://s2.glbimg.com/sjbIwYbTyJMCcO4JTs_yOSn-SBU=/620x480/e.glbimg.com/og/ed/f/original/2018/11/06/wave-3473335_1920.jpg',
+      dsc: 'Ocean'
+    },
+    {
+      url: 'https://ensia.com/wp-content/uploads/2017/06/feature_DeepOcean_mainNew-760x378.jpg',
+      dsc: 'Deep Ocean'
+    },
+    {
+      url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5al2-xvEjkkrkC2FnNKRxiJFdm9FzyXId7p_MYJrhmRC3hjWu9y9RUngNenIFOArQTS4&usqp=CAU',
+      dsc: 'Ominous Ocean'
+    }
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
+import { PhotosModule } from './photos/photos.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PhotoComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
